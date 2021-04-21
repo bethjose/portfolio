de1353aa7a9f2d09394aadc4f258f520c24cb54c
beth <bethanyrjose@gmail.com>
2021-04-22 11:32:41 +1200

    add bioinfo-scripts and test file

---
+++ bioinfo-scripts/fastaFormat.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+#common edits on fasta files
+#functions I would like:
+#trim fasta headers to length n or field n (with custom delimeter)
+#convert multi-line fasta to linear
+#rename fasta headers 1..n w/ option for n+filename/or other unique ID
